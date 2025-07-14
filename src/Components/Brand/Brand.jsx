5042af4f50854cb8cba6b7d4261c00e75c81e288
Amzad Hossain <amzadhrakib570@gmail.com>
2025-07-15 02:23:38 +0600

    bug fixed

--- src/Components/Brand/Brand.jsx
+++ src/Components/Brand/Brand.jsx
@@ -5,7 +5,7 @@ import { google, slack, atlassian, dropbox, shopify } from './import'
 
 const Brand = () => {
     return (
-        <div className='gpt3__brand section_padding'>
+        <div className='gpt3__brand section__padding'>
 
             <div>
                 <img src={google} alt="google" />
